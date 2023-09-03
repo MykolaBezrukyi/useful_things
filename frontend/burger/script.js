bc64117a2f66933d494340e822d9a3d6ac4e0f53
Mykola Bezrukyi <mykola_bezrukyi@epam.com>
2023-09-03 15:00:25 +0300

    add js code to burger template

---
+++ frontend/burger/script.js
@@ -0,0 +1,9 @@
+const menuIcon = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (menuIcon) {
+    menuIcon.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        menuIcon.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
